06029c72134349dbe4b62038f2fca71c05271d5b
Deepu <chandraswede@gmail.com>
2022-07-02 17:39:42 +0200

    More widgets!

--- GUIclass/GUIclass.py
+++ GUIclass/GUIclass.py
@@ -5,15 +5,23 @@ class RigGui:
         self.myFrame = Frame(master)
         self.myFrame.pack()
 
-        self.reqEnableText = Label(self.myFrame,text='Enable/Disable MCU',fg='Black',bg='White')
-        self.reqEnableText.grid(row=0,column=0)
+        self.CtrlModeText = Label(self.myFrame,text='Enter control mode')
+        self.CtrlModeText.grid(row=0,column=0)
+
+        self.TrqControl = Button(self.myFrame,text='Torque control')
+        self.TrqControl.grid(row=0,column = 1,padx=80)
+
+        self.SpdControl = Button(self.myFrame,text='Speed control')
+        self.SpdControl.grid(row=0,column=2,padx=80)
+
+        self.reqEnableText = Label(self.myFrame,text='Enable/Disable MCU')
+        self.reqEnableText.grid(row=1,column=0,pady=20)
 
         self.reqEnable = Button(self.myFrame,text='Enable MCU',command=self.clicker)
-        self.reqEnable.grid(row=0,column=1,padx=80)
+        self.reqEnable.grid(row=1,column=1,padx=80)
 
         self.reqDisable = Button(self.myFrame,text='Disable MCU',command=self.clicker)
-        self.reqDisable.grid(row=0,column=2,padx=10)
-
+        self.reqDisable.grid(row=1,column=2,padx=10)
 
     def clicker(self):
         print("Button clicked!")

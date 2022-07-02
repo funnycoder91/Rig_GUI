ee3362852f5264611dd6e06f095cc6fad5c5243b
Deepu <chandraswede@gmail.com>
2022-07-02 18:32:55 +0200

    Committing changes so I can go to the lake

--- GUIclass/GUIclass.py
+++ GUIclass/GUIclass.py
@@ -1,35 +1,45 @@
 from tkinter import *
 
 class RigGui:
-    def __init__(self,master):
+    def __init__(self,master,side):
         self.myFrame = Frame(master)
-        self.myFrame.pack()
+        self.myFrame.pack(side=side)
 
         self.CtrlModeText = Label(self.myFrame,text='Enter control mode:')
         self.CtrlModeText.grid(row=0,column=0)
 
         self.TrqControl = Button(self.myFrame,text='Torque control',command=self.TrqControlClicker)
-        self.TrqControl.grid(row=0,column = 1,padx=80)
+        self.TrqControl.grid(row=0,column = 1,padx=20)
 
-        self.SpdControl = Button(self.myFrame,text='Speed control')
-        self.SpdControl.grid(row=0,column=2,padx=80)
+        self.SpdControl = Button(self.myFrame,text='Speed control',command=self.SpdControlClicker)
+        self.SpdControl.grid(row=0,column=2,padx=20)
 
         self.reqEnableText = Label(self.myFrame,text='Enable/Disable MCU:')
         self.reqEnableText.grid(row=1,column=0,pady=20)
 
-        self.reqEnable = Button(self.myFrame,text='Enable MCU',command=self.clicker)
-        self.reqEnable.grid(row=1,column=1,padx=80)
+        self.reqEnable = Button(self.myFrame,text='Enable MCU',command=self.reqEnableClicker)
+        self.reqEnable.grid(row=1,column=1,padx=20)
 
-        self.reqDisable = Button(self.myFrame,text='Disable MCU',command=self.clicker)
-        self.reqDisable.grid(row=1,column=2,padx=10)
+        self.reqDisable = Button(self.myFrame,text='Disable MCU',command=self.reqDisableClicker)
+        self.reqDisable.grid(row=1,column=2,padx=20)
 
     def TrqControlClicker(self):
-        print("Torque control clicker!")
         self.TrqControl.config(bg='red')
         self.SpdControl.config(bg='white')
 
-    def clicker(self):
-        print("Clicked!")
+    def SpdControlClicker(self):
+        self.SpdControl.config(bg='red')
+        self.TrqControl.config(bg='white')
+
+    def reqEnableClicker(self):
+        self.reqEnable.config(bg='green')
+        self.reqDisable.config(bg='white')
+
+    def reqDisableClicker(self):
+        self.reqDisable.config(bg='green')
+        self.reqEnable.config(bg='white')
+
+
 
 
 
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from tkinter import *
 if __name__ == '__main__':
     root = Tk()
     root.title('Rig GUI')
-    root.geometry('700x350')
-    R1 = RigGui(root)
+    root.geometry('700x700')
+    R1 = RigGui(root,'top')
     root.mainloop()
 
 
